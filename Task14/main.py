15b525e54761afa6afa19d23b91f1052add45f05
vasilijchesnokov <vasilijchesnokov@yandex.ru>
2023-06-20 21:18:17 +0300

    task14 was modified

--- Task14/main.py
+++ Task14/main.py
@@ -2,12 +2,20 @@
 # Требуется вывести все целые степени двойки
 # (т.е. числа вида 2k), не превосходящие числа N.
 
-from random import randint
+# Вариант 1(мой)
+# from random import randint
+#
+# N = randint(1, 1024)
+# print(N, "->", end=' ')
+#
+# num_1 = 1
+# while num_1 <= N:
+#     print(num_1, end=' ')
+#     num_1 *= 2
 
-N = randint(1, 1024)
-print(N, "->", end=' ')
-
-num_1 = 1
-while num_1 < N:
-    print(num_1, end=' ')
-    num_1 *= 2
+# Вариант 2
+n = int(input('Enter the number: '))
+i = 0
+while 2**i <= n:
+    print(2**i, end=' ')
+    i += 1
